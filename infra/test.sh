b4a04041f78a66c01563b3392b53580737c998f2
Jay Conrod <jay@engflow.com>
2024-07-22 10:00:18 -0700

    temporary auth for re

--- infra/test.sh
+++ infra/test.sh
@@ -3,12 +3,16 @@
 set -o nounset -o pipefail -o errexit
 [[ "${SCRIPT_DEBUG:-"off"}" == "on" ]] && set -o xtrace
 
-if [ -z "${OS-}" ]; then
-  echo "OS must be set" >&2
-  exit 1
-fi
 if [ -z "${ARCH-}" ]; then
   echo "ARCH must be set" >&2
+  exit 1
+fi
+if [ -z "${OPAL_RPC_CREDENTIALS-}" ]; then
+  echo "OPAL_RPC_CREDENTIALS must be set" >&2
+  exit 1
+fi
+if [ -z "${OS-}" ]; then
+  echo "OS must be set" >&2
   exit 1
 fi
 
@@ -27,6 +31,23 @@ windows-x64)
   exit 1
 esac
 
+# TODO(CUS-356): use engflow_auth for authentication here.
+#
+# For now, the credential is stored in a GitHub secret as JSON, like this:
+#
+# {
+#   "headers": {
+#     "x-engflow-auth-method": ["jwt-v0"],
+#     "x-engflow-auth-token": ["long.example.token"]
+#   }
+# }
+jq <<<"${OPAL_RPC_CREDENTIALS}" >.bazelrc.user \
+  --raw-output \
+  '.headers | to_entries | map("build:opal_auth --remote_header=\(.key)=\(.value[0] | tostring)")[]'
+jq <<<"${OPAL_RPC_CREDENTIALS}" >>.bazelrc.user \
+  --raw-output \
+  '.headers | to_entries | map("build:opal_auth --bes_header=\(.key)=\(.value[0] | tostring)")[]'
+
 bazel_flags=(
   --config=noninteractive
   --config=opal
@@ -36,6 +57,6 @@ bazel_flags=(
 #`echo "Running Bazel tests..."
 bazel build "${bazel_flags[@]}" //...
 
-echo "Running Go tests..."
-bazel run @rules_go//go -- test ./...
-bazel run @rules_go//go -- clean -cache -modcache
+#echo "Running Go tests..."
+#bazel run @rules_go//go -- test ./...
+#bazel run @rules_go//go -- clean -cache -modcache
